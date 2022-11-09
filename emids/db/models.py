81765c75700b39cf2664ae0f79aff54e211b1daf
aviiciii <laavesh1@gmail.com>
2022-11-09 13:57:27 +0530

    db

--- emids/db/models.py
+++ emids/db/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+
+class Sex(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=6)
+
+    def __str__(self):
+        return self.name
+
+class Bloodgroup(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=4)
+
+    def __str__(self):
+        return self.name
+
+
 # Create your models here.
+class Patient(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    mobile = models.CharField(max_length=12)
+    sex= models.ForeignKey(Sex, on_delete=models.CASCADE)
+    age= models.CharField(max_length=100)
+    blood_group = models.ForeignKey(Bloodgroup, on_delete=models.CASCADE)
+    emergency_contact= models.CharField(max_length=12)
+    
+
+    def __str__(self):
+        return self.name
+
--- emids/patient/views.py
+++ emids/patient/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def dashboard(request):
+    return render(request, 'patient/dashboard.html')
+
+def search(request):
+    return render(request, 'patient/search.html')
+
+def patient_profile(request):
+    return render(request, 'patient/patient_profile.html')
