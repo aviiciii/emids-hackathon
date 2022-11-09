c4d5b5b69672acffb0a5f681ecf76bd5546ff81d
aviiciii <laavesh1@gmail.com>
2022-11-09 19:06:18 +0530

    db changes and migrations

--- emids/db/models.py
+++ emids/db/models.py
@@ -20,52 +20,49 @@ class Bloodgroup(models.Model):
 class Patient(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    mobile = models.CharField(max_length=12)
+    mobile = models.CharField(max_length=13)
     sex= models.ForeignKey(Sex, on_delete=models.CASCADE)
-    age= models.CharField(max_length=100)
+    age= models.CharField(max_length=5)
     blood_group = models.ForeignKey(Bloodgroup, on_delete=models.CASCADE)
-    emergency_contact= models.CharField(max_length=12)
+    emergency_contact= models.CharField(max_length=13)
     drinking=models.BooleanField(default=False)
     smoking=models.BooleanField(default=False)
-    past_history = models.CharField(maxlength=100)
-
+    past_history = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
+
 
 class Doctor(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     speciality= models.CharField(max_length=100)
-    mobile= models.CharField(max_length=12)
+    mobile= models.CharField(max_length=13)
 
     def __str__(self):
         return 'Dr.' + self.name
+
 
 class Appointment(models.Model):
     id = models.AutoField(primary_key=True)
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
     date = models.DateField()
-    time = models.TimeField()
-    examination-= models.ForeignKey("Examination", on_delete=models.CASCADE)
-
+    examination = models.ForeignKey("Examination", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.patient.name + ' ' + self.doctor.name
-
-
 
 
 class Medicine(models.Model):
     id=models.AutoField(primary_key=True)
     name=models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
 
 
-
-
-class Scan(models.Models):
+class Scan(models.Model):
     id=models.AutoField(primary_key=True)
     date=models.DateField()
     type=models.CharField(max_length=100)
@@ -73,33 +70,15 @@ class Scan(models.Models):
     def __str__(self):
         return self.id
 
-class Examination(models.Models):
+
+class Examination(models.Model):
     id=models.AutoField(primary_key=True)
-    scan=models.ForeignKey(Scan, on_delete=models.CASCADE)
     chief_complaint=models.CharField(max_length=100)
     present_illness=models.CharField(max_length=100)
     diagnosis=models.CharField(max_length=100)
-    Prescription=models.ForeignKey(Prescription, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.id
-
-
-
-class Prescription(models.Model):
-    id=models.AutoField(primary_key=True)
-    appointment=models.ForeignKey(Appointment, on_delete=models.CASCADE)
+    scan=models.ManyToManyField(Scan)
     medicine=models.ManyToManyField(Medicine)
 
     def __str__(self):
         return self.id
 
-
-
-
-
-
-
-
-
-    
