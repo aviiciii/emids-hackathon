db5cb26a50bbf6fddfa07186db70f2da5c711f35
aviiciii <laavesh1@gmail.com>
2022-11-09 11:53:01 +0530

    linked login

--- emids/login/views.py
+++ emids/login/views.py
@@ -9,6 +9,8 @@ def index(request):
 def patient_login(request):
     return render(request, 'login/patient_login.html')  
 
+
+
 # doctor login 
 def doctor_login(request):
     return render(request, 'login/doctor_login.html')
