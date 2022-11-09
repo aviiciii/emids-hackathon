b8da7e0c996c7dbe4cff06ad973fb7ef14151f24
aviiciii <laavesh1@gmail.com>
2022-11-09 12:29:32 +0530

    for ash

--- emids/login/views.py
+++ emids/login/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
 # homepage
 def index(request):
@@ -7,6 +8,21 @@ def index(request):
 
 # patient login
 def patient_login(request):
+    if request.method=="POST":
+        mobile = request.POST['mobile']
+        password = request.POST['password']
+        user = authenticate(request, mobile=mobile, password=password)
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.error(request, 'Invalid username and/or password.')
+            return redirect('login')
+
+
+
+
     return render(request, 'login/patient_login.html')  
 
 # doctor login 
