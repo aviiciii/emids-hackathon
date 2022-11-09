82ba55a63a8a91cfa9f3abfa26bd89163e2cbf56
aviiciii <laavesh1@gmail.com>
2022-11-09 19:38:21 +0530

    registered models

--- emids/db/admin.py
+++ emids/db/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Patient, Sex
+from .models import Patient, Sex, Bloodgroup, Doctor, Appointment, Examination, Medicine
 
 admin.site.register(Patient)
 admin.site.register(Sex)
+admin.site.register(Bloodgroup)
+admin.site.register(Doctor)
+admin.site.register(Appointment)
+admin.site.register(Examination)
+admin.site.register(Medicine)
