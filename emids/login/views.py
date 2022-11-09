58ec1652f694aea0fd1201567df18449e90b5fa1
aviiciii <laavesh1@gmail.com>
2022-11-09 09:47:44 +0530

    django init

---
+++ emids/login/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+# homepage
+def index(request):
+    return render(request, 'login/index.html')
+
+# patient login
+def patient_login(request):
+    return render(request, 'login/patient_login.html')  
+
+# doctor login 
+def doctor_login(request):
+    return render(request, 'login/doctor_login.html')
+    
