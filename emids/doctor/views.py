71c8f0513a2ec1a86273f20d8fa2f65218abd7af
aviiciii <laavesh1@gmail.com>
2022-11-09 20:51:14 +0530

    kolar finished

--- emids/doctor/views.py
+++ emids/doctor/views.py
@@ -6,7 +6,7 @@ def dash(request):
     # get the user
     username = request.user.get_username()
     # get the doctor
-    doctor = models.Doctor.objects.get(user__username=username)
+    doctor = models.Doctor.objects.get(id=username)
     
     # get the appointments
     appointments = models.Appointment.objects.filter(doctor=doctor, )
