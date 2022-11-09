b83fc5618867c922dbad49695aa728041167590e
aviiciii <laavesh1@gmail.com>
2022-11-09 11:02:39 +0530

    fuck

--- emids/doctor/urls.py
+++ emids/doctor/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlspatterns = [
-    path("", views.dash, name="doc_dash"),  
-     
+urlpatterns = [
+    # path("", views.dash, name="doc_dash"),  
 ]
  
