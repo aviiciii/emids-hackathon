add707c1db37659a01125ccd15dead726cdd1753
sasukexo <aswinash1002@gmail.com>
2022-11-09 10:43:04 +0530

    static added

--- emids/doctor/urls.py
+++ emids/doctor/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlspatterns = [
-    path("", views.index, name="index"),  
+    path("", views.index, name="doc_dash"),  
     path("")  
+]
  
--- emids/doctor/views.py
+++ emids/doctor/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-#
+
+def dash(request):
+    return render(request, 'login/dash.html')
+
+def  
