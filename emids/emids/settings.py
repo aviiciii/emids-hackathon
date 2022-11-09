98bbab379b6046498e51903e3dee3e2b15a674bf
aviiciii <laavesh1@gmail.com>
2022-11-09 10:33:23 +0530

    gitignore

--- emids/emids/settings.py
+++ emids/emids/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'db',
+    'doctor',
+    'patient',
+    'login',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
